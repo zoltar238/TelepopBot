e038e244b716070762aacfd5a0c7c1f68796c46a
zoltar238 <protoandrei@protonmail.com>
2024-09-30 14:28:57 +0200

    POM updates

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
-import Config.BotConfig;
-import Controller.TelegramController;
-import Model.ConfigCheckEnum.ConfigCheckEnum;
-import Util.ConfigChecker;
-import View.BadConfigView;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import Pages.WallapopUploadPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
+        /*
         BotConfig.initializeProperty();
         ConfigCheckEnum downloadPathCheck = ConfigChecker.checkDownloadPath();
         ConfigCheckEnum userDataCheck = ConfigChecker.checkUserData();
@@ -23,5 +24,31 @@ public class Main {
         } else {
             BadConfigView badConfigView = new BadConfigView(downloadPathCheck, userDataCheck, hashtagCheck);
         }
+         */
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\dabac\\Proton Drive\\Protoandrei\\My files\\TelepopBot\\src\\main\\resources\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("user-data-dir=" + "C:\\Users\\dabac\\AppData\\Local\\Google\\Chrome\\User Data");
+        chromeOptions.addArguments("profile-directory=" + "Default");
+        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+
+        driver.manage().window().maximize();
+
+        WallapopUploadPage walla = new WallapopUploadPage(driver, wait);
+
+        driver.get("https://es.wallapop.com/app/catalog/upload");
+
+
+        walla.clickProductType();
+        walla.enterTitle("hola");
+        walla.selectCategory();
+        walla.enterPrice();
+        walla.enterDescription();
+        walla.selectCondition();
+        walla.enterHashTags(new String[]{"HOla", "Walla"});
+        ArrayList<String> imagePaths = new ArrayList<>(Arrays.asList("C:\\Users\\dabac\\Proton Drive\\Protoandrei\\My files\\TelepopBot\\downloads\\xxasdxx\\xxasdxx1.jpg"));
+
+        walla.uploadImages(imagePaths);
+        walla.submit();
     }
 }
--- src/main/java/Pages/WallapopUploadPage.java
+++ src/main/java/Pages/WallapopUploadPage.java
@@ -4,15 +4,38 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+
+import static Config.BotConfig.properties;
+
 // page_url = https://es.wallapop.com/app/catalog/upload
 public class WallapopUploadPage {
 
     WebDriver driver;
     WebDriverWait wait;
 
-    By productTypeButton = By.xpath("\"//span[text()='Algo que ya no necesito']\"");
-    By titleTextBox = By.id("title");
-    By categorySelectorButton = By.xpath("\"//label[text()='Categoría y subcategoría']\"");
+    By productTypeOption = By.xpath("//span[text()='Algo que ya no necesito']");
+    By titleInputField = By.id("title");
+    By categorySelectorLabel = By.xpath("//label[text()='Categoría y subcategoría']");
+    //original
+    //WebElement panel1 = (WebElement) js.executeScript("return document.querySelector(\".sc-walla-dropdown-item-h.sc-walla-dropdown-item-s.hydrated:nth-child(11)\");");
+    By firstSubcategoryOption = By.cssSelector(".sc-walla-dropdown-item-h.sc-walla-dropdown-item-s.hydrated:nth-child(11)");
+    //original
+    //WebElement panel2 = (WebElement) js.executeScript("return document.querySelector(\".sc-walla-dropdown-item-h.sc-walla-dropdown-item-s.hydrated:nth-child(14)\");");
+    By secondSubcategoryOption = By.cssSelector(".sc-walla-dropdown-item-h.sc-walla-dropdown-item-s.hydrated:nth-child(14)");
+    By priceInputField = By.xpath("//input[@id='sale_price']");
+    By descriptionInputField = By.xpath("//textarea[@id='description']");
+    //original
+    //WebElement button = (WebElement) js.executeScript("return document.querySelectorAll('.walla-text-input__label.sc-walla-text-input')[8]");
+    By conditionSelector =  By.xpath("(//label[contains(@class, 'walla-text-input__label') and contains(@class, 'sc-walla-text-input')])[9]");
+    //original
+    //button = (WebElement) js.executeScript("return document.querySelectorAll('.sc-walla-dropdown-item-h.sc-walla-dropdown-item-s.hydrated')[19]");
+    By conditionSelectorOption = By.xpath("(//walla-dropdown-item[contains(@class, 'sc-walla-dropdown-item-h') and contains(@class, 'sc-walla-dropdown-item-s')])[20]");
+    By body = By.xpath("//tsl-multi-select-form[@class='HashtagField__suggested__multiselect ng-untouched ng-pristine ng-valid']");
+    By hashTagsInputField = By.cssSelector("input[placeholder='Buscar o crear hashtag']");
+    By hastTagsCheckBox = By.cssSelector(".Checkbox__mark.d-block.position-relative.m-0");
+    By submitButtonSelector =By.xpath("(//div[contains(@class, 'col-12 col-md-6')])[4]");
+
 
     public WallapopUploadPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
@@ -21,18 +44,67 @@ public class WallapopUploadPage {
 
     //select product type
     public void clickProductType() {
-        clickButton(productTypeButton, "Tipo de producto seleccionado correctamente", "No se ha entontrado el boton de tipo de producto");
+        clickButton(productTypeOption, "Tipo de producto seleccionado correctamente", "No se ha entontrado el boton de tipo de producto");
     }
 
     //enter title
     public void enterTitle(String title) {
-        enterText(titleTextBox, title, "Titulo insertado correctamente", "No se ha encontrado la caja de texto del titulo");
+        enterText(titleInputField, title, "Titulo insertado correctamente", "No se ha encontrado la caja de texto del titulo");
     }
 
     //open category item box
-    public void clickCategorySelector(){
-        clickButton(categorySelectorButton, "Caja de categorías abierta correctamente", "No se ha encontrado la caja de categorías");
+    public void selectCategory() {
+        clickButton(categorySelectorLabel, "Caja de categorías abierta correctamente", "No se ha encontrado la caja de categorías");
+        clickButton(firstSubcategoryOption, "Categoria seleccionada correctamente", "La categoría no pudo ser seleccionada");
+        clickButton(secondSubcategoryOption, "Subcategoria seleccionada correctamente", "La subcategoría no pudo ser seleccionada");
     }
+
+    //enter price
+    public void enterPrice() {
+        enterText(priceInputField, "1", "Precio añadido correctamente", "No se pudo añadir el precio");
+    }
+
+    //enter description
+    public void enterDescription() {
+        enterText(descriptionInputField, "Description", "Descripción añadida correctamente", "No se pudo añadir la descripción");
+    }
+
+    //select condition
+    public void selectCondition() {
+        clickButton(conditionSelector, "Menu de condición abierto correctamente", "No se pudo abrir el menu de condición");
+        clickButton(conditionSelectorOption, "Categoria seleccionada correctamente", "No se pudo seleccionar la categoria");
+    }
+
+    //enter hashTags
+    public void enterHashTags(String[] hashTags) {
+        for (String hashTag : hashTags) {
+            enterText(hashTagsInputField, hashTag, "Hashtag escrito correctamente", "Hashtag no se pudo escribir");
+            clickButton(hastTagsCheckBox, "Check seleccionado correctamente", "No se pudo selecionar el check");
+            clickButton(body, "Se ha añadido un hashtag correctamente", "No se pudo añadir el hastag");
+        }
+    }
+
+    //upload images
+    public void uploadImages(ArrayList<String> paths) {
+        for (String path : paths) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            WebElement fileInput = (WebElement) js.executeScript("return document.querySelector('.DropArea__wrapper input')");
+
+            fileInput.sendKeys(path);
+            //delay between image uploaded
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    //submit item
+    public void submit() {
+        clickButton(submitButtonSelector, "Item añadido correctamente", "No se pudo añadir el item");
+    }
+
     // method for entering text inside a text box
     private void enterText(By locator, String text, String successMessage, String errorMessage) {
         try {
