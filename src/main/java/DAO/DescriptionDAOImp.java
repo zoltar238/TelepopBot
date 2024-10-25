b73eea3391848de3d690b28961a0dfa079f10d4e
zoltar238 <protoandrei@protonmail.com>
2024-10-25 13:00:23 +0200

    bug fixes

--- src/main/java/DAO/DescriptionDAOImp.java
+++ src/main/java/DAO/DescriptionDAOImp.java
@@ -1,10 +1,24 @@
 package DAO;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DescriptionDAOImp implements DescriptionDAO{
     @Override
-    public String getDescription(File file) {
-        return "";
+    public String getDescription(String path) {
+        // extract description suffix from file
+        try {
+            // Read all lines from file
+            List<String> lines = Files.readAllLines(Paths.get(path));
+
+            // Write all lines to string
+            return String.join("\n", lines);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
